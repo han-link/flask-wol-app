83342e3665535811cfb99deb70bce2fa67b898ea
han-link <115926359+han-link@users.noreply.github.com>
2024-02-29 21:14:41 +0100

    modified wakeonlan

--- tests/wakeonlan-test.py
+++ tests/wakeonlan-test.py
@@ -4,6 +4,8 @@ from wake_net.wakeonlan import send_magic_packet
 
 load_dotenv()
 
-
 if __name__ == "__main__":
-    send_magic_packet(os.getenv('MAC_ADDRESS'))
+    try:
+        send_magic_packet(os.getenv("MAC_ADDRESS"))
+    except Exception as e:
+        print(f"Error sending magic packet: {e}")
--- wake_net/wakeonlan/__init__.py
+++ wake_net/wakeonlan/__init__.py
@@ -1,22 +1,21 @@
 import socket
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-BROADCAST_IP = '255.255.255.255'
-DEFAULT_PORT = 7
+def send_magic_packet(mac_address):
+    # Clean up this mac address convert from string to byte
+    mac_clean = mac_address.replace(":", "").replace("-", "")
+    mac_byte = bytes.fromhex(mac_clean)
 
+    # Create the payload 0xFF * 6 FF:FF: FF: FF: FF:FF + Mac address * 16
+    payload = b"\xFF" * 6 + mac_byte * 16
+    hex_string = ' '.join(f'{byte:02x}' for byte in payload)
+    print(hex_string)
 
-def create_magic_packet(macaddress: str) -> bytes:
-    macaddress = macaddress.replace(':', '')
-    if len(macaddress) != 12:
-        raise ValueError("Incorrect MAC address format")
-    return bytes.fromhex('FF' * 6 + macaddress * 16)
-
-
-def send_magic_packet(macaddress: str, ip_address: str = BROADCAST_IP, port: int = DEFAULT_PORT) -> None:
-    packet = create_magic_packet(macaddress)
+    # Send magic packet
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        try:
-            sock.sendto(packet, (ip_address, port))
-        except Exception as e:
-            print(f"Failed to send magic packet: {e}")
+
+        sock.sendto(payload, ("<broadcast>", 9))
